9350ceb1c80f81bcd3fe62a8597401bd3e9b6553
tane-simons <163289987+tane-simons@users.noreply.github.com>
2025-08-27 14:03:06 +1000

    add year/genre functionality, returns audio/artist/title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,63 @@
-const sliderValue = document.querySelector("#slider-value");
-const slider = document.querySelector("#slider");
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
+const titleContainer = document.getElementById("title-container");
+const artistContainer = document.getElementById("artist-container");
+const audioContainer = document.getElementById("audio-container");
+const pauseButton = document.getElementById("pause-button");
+let genre = "Jazz"; //preset to jazz
+document.getElementById("Jazz").checked = true; //show the radio jazz
+
+//decade selection
 sliderValue.textContent = slider.value;
-slider.addEventListener("input", (event) => {
-    sliderValue.textContent = event.target.value;
+slider.addEventListener("input", () => { //decade slider input
+    sliderValue.textContent = slider.value; //updates html
+    audio(slider.value, genre) //calls audio
 });
+
+//genre selection
+document.querySelectorAll("input[name='genre']").forEach(radio => {
+    radio.addEventListener("change", (event) => { //changin genres
+        genre = event.target.id; //storing genre
+        audio(slider.value, genre); //calls audio
+    });
+});
+
+//player func
+async function audio(year, genre) {
+    try {
+        //the api stuff
+        const api = `https://api.collection.nfsa.gov.au/search?query=&hasMedia=yes&mediaFileTypes=Audio&forms=Music&parentTitle.genres=${genre}%20music&year=${year}-${Number(year)+9}`;
+        const response = await fetch(api);
+        const data = await response.json();
+        const songs = data.results;
+
+        //get first matching song
+        const song1 = songs[0];
+        const audioPath = song1.preview[0].filePath;
+        const audioUrl = `https://media.nfsacollection.net/${audioPath}`;
+        
+        //display current song and info
+        const title = song1.title;
+        const artist = song1.credits[0].name;
+        titleContainer.innerHTML = `<p>${title}</p>`;
+        artistContainer.innerHTML = `<p>${artist}</p>`;
+        audioContainer.innerHTML = `<audio controls autoplay id="now-playing"><source src="${audioUrl}" type="audio/mpeg"></audio>`;
+    } catch (error) { //when no matching
+        titleContainer.innerHTML = "<p>No matches</p>";
+        audioContainer.innerHTML = "";
+    }
+}
+
+//pause func
+pauseButton.addEventListener("click", () => {
+    const nowPlaying = document.getElementById("now-playing")
+    if (!nowPlaying.paused) { //audio is playing
+        nowPlaying.pause();
+        pauseButton.textContent = "PLAY";
+    } else {
+        nowPlaying.play();
+        pauseButton.textContent = "PAUSE";
+    }
+});
+
+audio(slider.value, genre);
